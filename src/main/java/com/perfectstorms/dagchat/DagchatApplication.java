43fd5e7f5ed1e409dd2b4b05410a88743bf85423
mamadaliev <mamadaliev@yahoo.com>
2019-06-17 01:39:21 +0300

    Initial project

---
+++ src/main/java/com/perfectstorms/dagchat/DagchatApplication.java
@@ -0,0 +1,13 @@
+package com.perfectstorms.dagchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagchatApplication.class, args);
+    }
+
+}
